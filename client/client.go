fad2ad95243e188b4e2c772425e58d49c398b677
IDarar <nfjn54@mail.ru>
2021-05-23 19:58:36 +0300

    changed run func

--- client/client.go
+++ client/client.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"context"
 	"log"
 
 	"github.com/IDarar/hub/pkg/logger"
 	"github.com/IDarar/notifications-service/internal/config"
-	"github.com/IDarar/notifications-service/pb"
 	"github.com/IDarar/notifications-service/pkg/tlscredentials"
-	"github.com/golang/protobuf/ptypes/timestamp"
 	"google.golang.org/grpc"
 )
-
-var ctx = context.Background()
 
 func main() {
 	cfg, err := config.Init("configs/main")
@@ -32,17 +27,4 @@ func main() {
 		log.Fatal("cannot dial server: ", err)
 	}
 	defer conn.Close()
-	c := pb.NewNotificationsServiceClient(conn)
-
-	ts := &timestamp.Timestamp{Seconds: 10, Nanos: 10}
-
-	res := &pb.Notification{To: "1241", From: "124", Time: ts, Where: "RTE", Content: "Hi", Type: "forum not"}
-
-	r, err := c.NotificationCreate(ctx, res)
-
-	if err != nil {
-		logger.Error("could not perform req ", err)
-		return
-	}
-	logger.Info(r)
 }
